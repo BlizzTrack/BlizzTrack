0806550a65711868d0a47fe03805d8c548c25125
Helba <Toyz@users.noreply.github.com>
2020-12-14 11:58:16 -0800

    natural string sorting

---
+++ BlizzTrack/Extensions/ListExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace BlizzTrack.Extensions
+{
+    public static class ListExtensions
+    {
+        public static IOrderedEnumerable<T> OrderByAlphaNumeric<T>(this IEnumerable<T> source, Func<T, string> selector) { 
+            int max = source.SelectMany(i => Regex.Matches(selector(i), @"\d+").Cast<Match>().Select(m => (int?)m.Value.Length)).Max() ?? 0; 
+            return source.OrderBy(i => Regex.Replace(selector(i), @"\d+", m => m.Value.PadLeft(max, '0'))); 
+        }
+    }
+}
