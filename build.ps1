5111351b85ba7b6b6a7e652600d782c3176ed6a6
Helba <toyzz@hotmail.com>
2020-12-17 15:29:47 -0800

    better build script

--- build.ps1
+++ build.ps1
@@ -1,10 +1,21 @@
-param ($param1)
+param ($param2, $param1)
 if(!$param1)  {
 	$param1 = "develop";
 }
 
-docker build --file '.\BlizzTrack\Dockerfile' . --tag registry.gitlab.com/blizztrack/blizztrack-v2:$param1
-docker build --file '.\Worker\Dockerfile' . --tag registry.gitlab.com/blizztrack/blizztrack-v2/worker:$param1
-
-docker push registry.gitlab.com/blizztrack/blizztrack-v2:$param1
-docker push registry.gitlab.com/blizztrack/blizztrack-v2/worker:$param1
+Switch ($param2) {
+	"worker" {
+		docker build --file '.\Worker\Dockerfile' . --tag registry.gitlab.com/blizztrack/blizztrack-v2/worker:$param1
+		docker push registry.gitlab.com/blizztrack/blizztrack-v2/worker:$param1
+	}
+	"site" {
+		docker build --file '.\BlizzTrack\Dockerfile' . --tag registry.gitlab.com/blizztrack/blizztrack-v2:$param1
+		docker push registry.gitlab.com/blizztrack/blizztrack-v2:$param1
+	}
+	default {
+		docker build --file '.\Worker\Dockerfile' . --tag registry.gitlab.com/blizztrack/blizztrack-v2/worker:$param1
+		docker push registry.gitlab.com/blizztrack/blizztrack-v2/worker:$param1
+		docker build --file '.\BlizzTrack\Dockerfile' . --tag registry.gitlab.com/blizztrack/blizztrack-v2:$param1
+		docker push registry.gitlab.com/blizztrack/blizztrack-v2:$param1
+	}
+}
