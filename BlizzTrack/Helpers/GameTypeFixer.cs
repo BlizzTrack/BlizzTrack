b6d1ba83d66edc1c54f3c02d23b4b4b20a5a5860
Helba The AI <toyzz@live.com>
2021-01-15 11:16:49 -0800

    fixed game name

---
+++ BlizzTrack/Helpers/GameTypeFixer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlizzTrack.Helpers
+{
+    public static class GameTypeFixer
+    {
+        private static Dictionary<string, string> Names = new Dictionary<string, string>
+        {
+            { "version", "Version" },
+            { "versions", "Versions" },
+            { "cdn", "CDN" },
+            { "cdns", "CDNs" },
+            { "bgdl", "BGDL" },
+            { "experimental", "Experimental" },
+            { "retail", "Retail" },
+            { "live", "Live" },
+            { "ptr", "PTR" },
+            { "beta", "Beta" },
+        };
+
+        public static string Fix(string type)
+        {
+            if(Names.ContainsKey(type.ToLower()))
+            {
+                return Names[type.ToLower()];
+            }
+
+            return type;
+        }
+    }
+}
