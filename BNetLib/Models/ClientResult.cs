b49341ebd7fe24f3f0e9561ce5945188fce26182
Helba The AI <toyzz@live.com>
2021-01-24 10:24:19 -0800

    minor changes

--- BNetLib/Models/ClientResult.cs
+++ BNetLib/Models/ClientResult.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace BNetLib.Models
 {
     public class ClientResult<T> where T : NGPD, new()
     {
         public int Seqn { get; set; }
+
         public string Raw { get; set; }
 
         public IList<T> Payload { get; set; }
