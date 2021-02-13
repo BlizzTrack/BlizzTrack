47cc8129a6aa8884c6a545ed00406f03c3bbaa95
Helba The AI <toyzz@live.com>
2021-02-13 10:00:42 -0800

    minor fixes

--- BNetLib.test/BNetTools.cs
+++ BNetLib.test/BNetTools.cs
@@ -14,7 +14,7 @@ Region!STRING:0|BuildConfig!HEX:16|CDNConfig!HEX:16|KeyRing!HEX:16|BuildId!DEC:4
 us|bc968f5914f05bc6b9ff67f6ed63a4d0|fb5eb6011c94e9d32e5e7c4124e416f5||8512817|bo5_TU8_8512817_2_ship|a9d8e0ab0615fcc2a919bbe840493de4
 eu|b3585b8f0cbf9040ffb68d313a59c2c4|fb5eb6011c94e9d32e5e7c4124e416f5||8408002|bo5_TU7_8408002J_signed_dev_esports|a9d8e0ab0615fcc2a919bbe840493de4
 kr|20745522b883e9616743d5092e479fae|fb5eb6011c94e9d32e5e7c4124e416f5||8408002|bo5_TU7_8408002J_signed_dev|a9d8e0ab0615fcc2a919bbe840493de4")]
-        public void Parse(string data)
+        public void VersionParse(string data)
         {
             var res = BNetLib.Networking.BNetTools.Parse<BNetLib.Models.Versions>(data.Split("\n"));
 
@@ -28,12 +28,45 @@ Region!STRING:0|BuildConfig!HEX:16|CDNConfig!HEX:16|KeyRing!HEX:16|BuildId!DEC:4
 us|bc968f5914f05bc6b9ff67f6ed63a4d0|fb5eb6011c94e9d32e5e7c4124e416f5||8512817|bo5_TU8_8512817_2_ship|a9d8e0ab0615fcc2a919bbe840493de4
 eu|b3585b8f0cbf9040ffb68d313a59c2c4|fb5eb6011c94e9d32e5e7c4124e416f5||8408002|bo5_TU7_8408002J_signed_dev_esports|a9d8e0ab0615fcc2a919bbe840493de4
 kr|20745522b883e9616743d5092e479fae|fb5eb6011c94e9d32e5e7c4124e416f5||8408002|bo5_TU7_8408002J_signed_dev|a9d8e0ab0615fcc2a919bbe840493de4")]
-        public void Reader(string data)
+        public void VersionBuildId(string data)
         {
             var res = BNetLib.Networking.BNetTools.Parse<BNetLib.Models.Versions>(data.Split("\n"));
 
-            var first = res.Value.First();
-            Assert.IsTrue(first.Versionsname == "bo5_TU8_8512817_2_ship", $"Failed {nameof(first.Versionsname)} got {first.Versionsname} wanted bo5_TU8_8512817_2_ship");
+            var first = res.Value.FirstOrDefault(x => x.Region == "us");
+            Assert.IsTrue(first.Buildid == 8512817, $"Failed {nameof(first.Buildid)} got {first.Buildid} wanted 8512817");
+        }
+
+        [TestMethod]
+        [DataRow(@"## nothing
+Name!STRING:0|Path!STRING:0|Hosts!STRING:0|Servers!STRING:0|ConfigPath!STRING:0
+## seqn = 384642
+eu|tpr/ovw|level3.blizzard.com eu.cdn.blizzard.com|http://eu.cdn.blizzard.com/?maxhosts=4 http://level3.blizzard.com/?maxhosts=4 https://blzddist1-a.akamaihd.net/?fallback=1&maxhosts=4 https://eu.cdn.blizzard.com/?fallback=1&maxhosts=4 https://level3.ssl.blizzard.com/?fallback=1&maxhosts=4|tpr/configs/data
+kr|tpr/ovw|level3.blizzard.com kr.cdn.blizzard.com blizzard.gcdn.cloudn.co.kr|http://blizzard.gcdn.cloudn.co.kr/?maxhosts=4 http://kr.cdn.blizzard.com/?maxhosts=4 http://level3.blizzard.com/?maxhosts=4 https://blizzard.gcdn.cloudn.co.kr/?fallback=1&maxhosts=4 https://blzddistkr1-a.akamaihd.net/?fallback=1&maxhosts=4 https://kr.cdn.blizzard.com/?fallback=1&maxhosts=4 https://level3.ssl.blizzard.com/?fallback=1&maxhosts=4|tpr/configs/data
+cn|tpr/ovw|client02.pdl.wow.battlenet.com.cn client05.pdl.wow.battlenet.com.cn|http://client02.pdl.wow.battlenet.com.cn/?maxhosts=4 http://client05.pdl.wow.battlenet.com.cn/?maxhosts=4 https://blzddist1-a.akamaihd.net/?fallback=1&maxhosts=4 https://client02.pdl.wow.battlenet.com.cn/?fallback=1&maxhosts=4 https://client05.pdl.wow.battlenet.com.cn/?fallback=1&maxhosts=4|tpr/configs/data
+us|tpr/ovw|level3.blizzard.com us.cdn.blizzard.com|http://level3.blizzard.com/?maxhosts=4 http://us.cdn.blizzard.com/?maxhosts=4 https://blzddist1-a.akamaihd.net/?fallback=1&maxhosts=4 https://level3.ssl.blizzard.com/?fallback=1&maxhosts=4 https://us.cdn.blizzard.com/?fallback=1&maxhosts=4|tpr/configs/data
+tw|tpr/ovw|level3.blizzard.com us.cdn.blizzard.com|http://level3.blizzard.com/?maxhosts=4 http://us.cdn.blizzard.com/?maxhosts=4 https://blzddist1-a.akamaihd.net/?fallback=1&maxhosts=4 https://level3.ssl.blizzard.com/?fallback=1&maxhosts=4 https://us.cdn.blizzard.com/?fallback=1&maxhosts=4|tpr/configs/data")]
+        public void CDNParse(string data)
+        {
+            var res = BNetLib.Networking.BNetTools.Parse<BNetLib.Models.CDN>(data.Split("\n"));
+
+            var first = res.Value.FirstOrDefault(x => x.Name == "us");
+            Assert.IsTrue(first.Hosts == "level3.blizzard.com us.cdn.blizzard.com", $"Failed {nameof(first.Hosts)} got {first.Hosts} wanted level3.blizzard.com us.cdn.blizzard.com");
+        }
+
+        [TestMethod]
+        [DataRow(@"## nothing
+Name!STRING:0|Path!STRING:0|Hosts!STRING:0|Servers!STRING:0|ConfigPath!STRING:0
+## seqn = 384642
+eu|tpr/ovw|level3.blizzard.com eu.cdn.blizzard.com|http://eu.cdn.blizzard.com/?maxhosts=4 http://level3.blizzard.com/?maxhosts=4 https://blzddist1-a.akamaihd.net/?fallback=1&maxhosts=4 https://eu.cdn.blizzard.com/?fallback=1&maxhosts=4 https://level3.ssl.blizzard.com/?fallback=1&maxhosts=4|tpr/configs/data
+kr|tpr/ovw|level3.blizzard.com kr.cdn.blizzard.com blizzard.gcdn.cloudn.co.kr|http://blizzard.gcdn.cloudn.co.kr/?maxhosts=4 http://kr.cdn.blizzard.com/?maxhosts=4 http://level3.blizzard.com/?maxhosts=4 https://blizzard.gcdn.cloudn.co.kr/?fallback=1&maxhosts=4 https://blzddistkr1-a.akamaihd.net/?fallback=1&maxhosts=4 https://kr.cdn.blizzard.com/?fallback=1&maxhosts=4 https://level3.ssl.blizzard.com/?fallback=1&maxhosts=4|tpr/configs/data
+cn|tpr/ovw|client02.pdl.wow.battlenet.com.cn client05.pdl.wow.battlenet.com.cn|http://client02.pdl.wow.battlenet.com.cn/?maxhosts=4 http://client05.pdl.wow.battlenet.com.cn/?maxhosts=4 https://blzddist1-a.akamaihd.net/?fallback=1&maxhosts=4 https://client02.pdl.wow.battlenet.com.cn/?fallback=1&maxhosts=4 https://client05.pdl.wow.battlenet.com.cn/?fallback=1&maxhosts=4|tpr/configs/data
+us|tpr/ovw|level3.blizzard.com us.cdn.blizzard.com|http://level3.blizzard.com/?maxhosts=4 http://us.cdn.blizzard.com/?maxhosts=4 https://blzddist1-a.akamaihd.net/?fallback=1&maxhosts=4 https://level3.ssl.blizzard.com/?fallback=1&maxhosts=4 https://us.cdn.blizzard.com/?fallback=1&maxhosts=4|tpr/configs/data
+tw|tpr/ovw|level3.blizzard.com us.cdn.blizzard.com|http://level3.blizzard.com/?maxhosts=4 http://us.cdn.blizzard.com/?maxhosts=4 https://blzddist1-a.akamaihd.net/?fallback=1&maxhosts=4 https://level3.ssl.blizzard.com/?fallback=1&maxhosts=4 https://us.cdn.blizzard.com/?fallback=1&maxhosts=4|tpr/configs/data")]
+        public void CDNSeqnParse(string data)
+        {
+            var res = BNetLib.Networking.BNetTools.Parse<BNetLib.Models.CDN>(data.Split("\n"));
+
+            Assert.IsTrue(res.Seqn == 384642, $"Failed {nameof(res.Seqn)} got {res.Seqn} wanted 384642");
         }
     }
 }   
--- BNetLib/Networking/BNetTools.cs
+++ BNetLib/Networking/BNetTools.cs
@@ -1,27 +1,48 @@
 ﻿using BNetLib.Extensions;
 using BNetLib.Models;
-using Newtonsoft.Json;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace BNetLib.Networking
 {
+    internal enum KeyTypeEnum
+    {
+        DEC,
+        STRING,
+        HEX
+    }
+
+    /// <summary>
+    ///     This is a Key, Type system. So this let's you see what type is linked to what key
+    ///     EX: "BuildId!DEC:4", this in-turn becomes BuildId -> Dec
+    /// </summary>
     internal class KeyType
     {
-        public KeyType(string type, string key)
+        public KeyType () { }
+
+        public KeyType(KeyTypeEnum type, string key)
         {
             Type = type;
             Key = key;
         }
 
+        /// <summary>
+        ///     Just the Key name
+        /// </summary>
         public string Key { get; }
-        public string Type { get; }
+
+        /// <summary>
+        ///     Just the type ex: DEC (int)
+        /// </summary>
+        public KeyTypeEnum Type { get; }
     }
 
     public static class BNetTools
     {
+        // TODO: Optimize this some more, there is some issues with how I designed this system
+        //       the problem is we have two different loops in this code... One to load the keys and there respected types
+        //       other is to parse this data manfest... We could make this smarter some how I think
         public static (IList<T> Value, int Seqn) Parse<T>(IEnumerable<string> lines) where T : NGPD, new()
         {
             var dataItems = new List<T>();
@@ -38,7 +59,7 @@ namespace BNetLib.Networking
                 var item = key.Split("!");
                 var itemType = item.Last().Split(":").First();
 
-                keys.Add(new KeyType(itemType, item.First()));
+                keys.Add(new KeyType((KeyTypeEnum)Enum.Parse(typeof(KeyTypeEnum), itemType, true), item.First()));
             }
 
             foreach (var line in enumerable.Skip(2))
@@ -62,19 +83,22 @@ namespace BNetLib.Networking
                     var key = keys[i];
                     var item = values[i].Trim();
 
-                    if (key.Type.Equals("dec", StringComparison.CurrentCultureIgnoreCase))
+                    switch(key.Type)
                     {
-                        _ = int.TryParse(item, out var seqn1);
-                        lineItem[key.Key] = seqn1;
-                        continue;
-                    }
-
-                    if (key.Key.ToLower() == "flags")
-                    {
-                        lineItem[key.Key] = item == "" ? "versions" : item.Trim();
-                    } else
-                    {
-                        lineItem[key.Key] = item == "" ? "" : item.Trim();
+                        case KeyTypeEnum.DEC:
+                            _ = int.TryParse(item, out var seqn1);
+                            lineItem[key.Key] = seqn1;
+                            continue;
+                        case KeyTypeEnum.HEX or KeyTypeEnum.STRING:
+                            if (key.Key.ToLower() == "flags")
+                            {
+                                lineItem[key.Key] = item == "" ? "versions" : item.Trim();
+                            }
+                            else
+                            {
+                                lineItem[key.Key] = item == "" ? "" : item.Trim();
+                            }
+                            break;
                     }
                 }
 
