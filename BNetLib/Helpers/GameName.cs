af2911de408e93b67c528ce662221ccf9ff41aff
Helba <helba@netslum.dev>
2021-03-22 18:28:09 -0700

    bug fixes

--- BNetLib/Helpers/GameName.cs
+++ BNetLib/Helpers/GameName.cs
@@ -145,7 +145,7 @@ namespace BNetLib.Helpers
                 break;
             }
 
-            return name == "" ? code : name;
+            return string.IsNullOrEmpty(name) ? code : name;
         }
     }
 }
